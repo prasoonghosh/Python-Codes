54a288a985504c21dd8b57f7209f74ccea3cf186
Prasoon Ghosh <prasoonpg.ghosh@gmail.com>
2022-05-27 08:36:56 +0530

    Created my first function

---
+++ greeting.py
@@ -0,0 +1,7 @@
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+
+
+greeting("Blake", "IT Support")
+greeting("Elis", "Software Engineering")
