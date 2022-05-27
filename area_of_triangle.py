70dc54ba751c997bebf188f73f19992e4defcc96
Prasoon Ghosh <prasoonpg.ghosh@gmail.com>
2022-05-27 08:54:29 +0530

    Learning to return values in functions

---
+++ area_of_triangle.py
@@ -0,0 +1,7 @@
+def area_of_triangle(base, height):
+    return base*height/2
+
+area_a = area_of_triangle(5,4)
+area_b = area_of_triangle(7,3)
+sum = area_a + area_b
+print("The sum of the triangle is: " +str(sum))
