7824fe77c8bbe7d76b05cc666ca9dfd897875a96
prasoonghosh <prasoonpg.ghosh@gmail.com>
2022-06-14 11:34:04 +0530

    Initial Commit

---
+++ Strings.py
@@ -0,0 +1,49 @@
+# color = "Orange"
+# print(color[1:4])
+# print(color[:4])
+# print((color[4:]))
+
+# message = "A kong string with a silly typo"
+# new_message = message[0:2] + "l" + message[3:]
+# print(new_message)
+# message = "This is a new message"
+# print(message)
+# message = "And another one"
+# print(message)
+
+# pets = "Cats & Dogs"
+# print(pets.index("&"))
+# print(pets.index("C"))
+# print(pets.index("Dog"))
+# print(pets.index("s"))
+
+# print("Dragons" in pets)
+# print("Cats" in pets)
+
+# def replace_domain(email, old_domain, new_domain):
+#     if "@" + old_domain in email:
+#         index = email.index("@" + old_domain)
+#         new_email = email[:index] + "@" + new_domain
+#         return new_email
+#     return email
+
+# print("mountains".upper())
+# print("MOUNTAINS".lower())
+# print("          Yes                 ".strip())
+
+# a = ["This", "is", "a", "string", "joined", "by", "spaces"]
+# print(" ".join(a))
+# a = ["This", "is", "a", "string", "joined", "by", "triple", "dots"]
+# print("...".join(a))
+# a = "This is another example"
+# print(a.split())
+
+# name = input("Enter Your name: ")
+# number = len(name) * 3
+# print("Hello {}, your lucky number is {}".format(name, number))
+# print("Your lucky number is {}, {}".format(number, name))
+
+# price = int(input("Enter the price of the product(in Rs.): "))
+# with_tax = price * 1.09
+# print("Base price: Rs.{:.2f}. With Tax: Rs.{:.2f}".format(price, with_tax))
+
