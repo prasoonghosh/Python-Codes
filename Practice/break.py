347517bc859314063c5cb4bde732f1b64a82da08
prasoonghosh <prasoonpg.ghosh@gmail.com>
2022-06-15 13:40:11 +0530

    Random Commit

---
+++ Practice/break.py
@@ -0,0 +1,7 @@
+def Break():
+    x = 0
+    while x < 3:
+        x = x +1
+        break
+    print(x)
+Break()
--- Practice/while-loop.py
+++ Practice/while-loop.py
@@ -15,10 +15,3 @@ def sum(x):
         print(False)
 sum(int(input("Enter num: ")))
 
-def Break():
-    x = 0
-    while x < 3:
-        x = x +1
-        break
-    print(x)
-Break()
