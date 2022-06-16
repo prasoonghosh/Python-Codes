e4fcd96c8b28f01f6df1be17ae6ffba482871c03
prasoonghosh <prasoonpg.ghosh@gmail.com>
2022-06-16 18:35:18 +0530

    Added while-loops2.py and while-initialize.py and modifies while-loops.py

---
+++ Coursera Codes/while-loops-2.py
@@ -0,0 +1,15 @@
+x = 1
+sum = 0
+while x < 10:
+    x += 1
+    sum += x
+    print("The value of x is: "+ str(x) + " and the value of variable sum is: "+str(sum))
+print("The final value of x is: "+ str(x) + " and the final sum is: " +str(sum))
+
+product = 1
+x = 1
+while x < 10:
+	product = product * x
+	x += 1
+
+print(sum, product)
--- Coursera Codes/while-loops.py
+++ Coursera Codes/while-loops.py
@@ -2,20 +2,13 @@
 # while x < 5:
 #     print("Not there yet, x = " +str(x))
 #     x = x + 1
-# print("x = "+str(x))
+# print("x = " + str(x))
 
-# def attempts(n):
-#     x = 1
-#     while x <= n:
-#         print("Attempt: " + str(x))
-#         x += 1
-#     print("Done")
-# attempts(5)
+#def attempts(n):
+#    x = 1
+#    while x <= n:
+#        print("Attempt: " + str(x))
+#        x += 1
+#    print("Done")
+#attempts(5)
 
-# x = 1
-# sum = 0
-# while x < 10:
-#     x += 1
-#     sum += x
-#     print("The value of x is: "+ str(x) + " and the value of variable sum is: "+str(sum))
-# print("The final value of x is: "+ str(x) + " and the final sum is: " +str(sum))
