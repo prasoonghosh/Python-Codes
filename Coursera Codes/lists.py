d3ebdf7fbacbe93ce0123e63b7d0014892dae83b
prasoonghosh <prasoonpg.ghosh@gmail.com>
2022-06-19 19:28:57 +0530

    Used various list methods

--- Coursera Codes/lists.py
+++ Coursera Codes/lists.py
@@ -1,3 +1,17 @@
-x = ["Now", "we", "are", "cooking!"]
-print(type(x))
-print(x)
+# x = ["Now", "we", "are", "cooking!"]
+# print(type(x))
+# print(x)
+
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+print(fruits)
+fruits.append("Kiwi")
+print(fruits)
+fruits.insert(0, "Orange")
+print(fruits)
+fruits.insert(25, "Peach")
+fruits.remove("Melon")
+print(fruits)
+fruits.pop(3)
+print(fruits)
+fruits[2] = "Strawberry"
+print(fruits)
